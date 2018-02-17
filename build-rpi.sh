c56d6375853dd2393ff5f4263fe452cc88a2c7f8
xzx <xuzhenxing@canaan-creative.com>
2018-02-17 16:08:09 +0800

    Add build-rpi.sh

---
+++ build-rpi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#CORE_NUM=13 AVA_MACHINE=avalon7 AVA_TARGET_BOARD=xc7z100 ./build-avalon-image.sh --build
+#CORE_NUM=13 AVA_MACHINE=avalon8 AVA_TARGET_BOARD=rpi2-modelb ./build-avalon-image.sh --build
+
+CORE_NUM=13 AVA_MACHINE=avalon8 ./build-avalon-image.sh --build
