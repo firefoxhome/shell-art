a617918ca93bc820f23c6abf881ee25e6f5aeddb
xzx <xuzhenxing@canaan-creative.com>
2018-03-29 16:12:07 +0800

    Update get-rpi-debian-datas directory

--- get-rpi-debian-datas/get-rpi-debian-datas.sh
+++ get-rpi-debian-datas/get-rpi-debian-datas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Get raspberry debian system datas
 
-for ip in `cat network.config | awk 'NR > 1'`;
+for ip in `cat network.conf | awk 'NR > 1'`;
 do
     echo "raspberry ip address = $ip"
     ./scp-login.exp $ip
