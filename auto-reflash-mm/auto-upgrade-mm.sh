8e14353af9b9d8caf675c15d15197160c3b2303e
xzx <xuzhenxing@canaan-creative.com>
2018-04-09 11:58:37 +0800

    Update auto-upgrade-mm.sh

--- auto-reflash-mm/auto-upgrade-mm.sh
+++ auto-reflash-mm/auto-upgrade-mm.sh
@@ -3,37 +3,56 @@
 # automatic upgrade MM firmware, if it has the lastest mm version
 #
 
-# Download changelog
-type=`cgminer-api estats | sed 's/DNA/\n/g' | grep '\=Ver' | sed 's/\[/\n\[/g' | grep '\[' | sed 's/\]//g' | sed 's/\[//g' | cut -b 1-3`
-if [ $type == '821' ]; then
-    avalon_type=avalon821
-elif [$type == '841' ]; then
-    avalon_type=avalon841
-fi
-wget https://canaan.io/downloads/software/${avalon_type}/mm/latest/changelog
-remote=`cat changelog | grep 'Version' | awk '{ print $2 }'`
+mm821_flag=0
+mm841_flag=0
 
-# Compare MM version
-for local in `cgminer-api estats | sed 's/DNA/\n/g' | grep '\=Ver' | sed 's/\[/\n\[/g' | grep '\[' | sed 's/\]//g' | sed 's/\[//g'`
+for local in `cgminer-api estats | sed 's/DNA/\nDNA/g' | sed 's/\=Ver/\n\=Ver/g' | grep '\=Ver' | sed 's/\=Ver\[//g' | sed 's/\]//g'`
 do
-    echo "$local", "$remote"
+    type=`echo $local | cut -b 1-3`
+    if [ $type == '821' ]; then
+        avalon_type=avalon821
+	if [ $mm821_flag -eq 0 ]; then
+		mm821_flag=1
+	else
+		continue
+	fi
+    elif [ $type == '841' ]; then
+        avalon_type=avalon841
+	if [ $mm841_flag -eq 0 ]; then
+		mm841_flag=1
+	else
+		continue
+	fi
+    fi
+
+    # Download changelog
+    wget https://canaan.io/downloads/software/${avalon_type}/mm/latest/changelog
+    remote=`cat changelog | grep 'Version' | awk '{ print $2 }'`
+
+    # Compare MM version
     if [ $local == $remote ]; then
         echo "Don't need reflash firmware version."
 	rm changelog
-        exit
+        continue
     else
         wget https://canaan.io/downloads/software/${avalon_type}/mm/latest/MM${type}.mcs
         wget https://canaan.io/downloads/software/${avalon_type}/mm/latest/md5sums
     fi
+
+    # MD5sum compare and upgrade mm firmware
+    ck_mm=`md5sum MM${type}.mcs`
+    ck_md5=`cat md5sums | grep 'mcs'`
+    if [ "$ck_mm" == "$ck_md5" ]; then
+	mv MM${type}.mcs mm.mcs
+	mv mm.mcs /tmp/
+	/usr/bin/mmupgrade
+	echo "upgrade $type firmware"
+    fi
+
+    # Delete download files
+    rm changelog md5sums /tmp/mm.mcs
 done
 
-# MD5sum compare and upgrade mm firmware
-ck_mm=`md5sum MM${type}.mcs`
-ck_md5=`cat md5sums | grep 'mcs'`
-if [ "$ck_mm" != "$ck_md5" ]; then
-    mv MM${type}.mcs mm.mcs
-    mv mm.mcs /tmp/
-    /usr/bin/mmupgrade
-fi
-
-rm changelog md5sums /tmp/mm.mcs
+# Clear flag
+mm821_flag=0
+mm841_flag=0
