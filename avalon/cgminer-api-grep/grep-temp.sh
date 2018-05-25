a951bb31332421cd5ab56f7ea8e178817b0cbdc5
xzx <xuzhenxing@canaan-creative.com>
2018-05-25 16:07:17 +0800

    Cleanup

--- avalon/cgminer-api-grep/grep-temp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat cgminer-log-message | sed 's/Temp\[/\nTemp\[/g' | sed 's/Fan/\n/g' | grep "Temp\[" > temp-file
-
-cat temp-file | sed 's/\[/ /g' | sed 's/\]/ /g' > temp-grep
-
-rm temp-file
--- avalon/cgminer-api-grep/grep-wu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat cgminer-log-message | sed 's/WU/\nWU/g' | sed 's/Freq/\n/g' | grep "WU" > wu-file
-
-cat wu-file | sed 's/\[/ /g' | sed 's/\]/ /g' > wu-grep
-
-rm wu-file
